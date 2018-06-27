b1e69fb2d6c9b1215ef5384a9f5c8bc66edbd0ce
zu-ann <asl29112@gmail.com>
2018-06-27 22:57:24 +0300

    first

--- pytsort/main.py
+++ pytsort/main.py
@@ -2,6 +2,9 @@
 
 import sys
 import argparse
+import re
+from collections import defaultdict
+
 
 parser = argparse.ArgumentParser(description='perform topological sort')
 
@@ -10,6 +13,40 @@ parser.add_argument('infile', nargs='?', metavar='FILE',
 
 parser.add_argument('--version', dest='version', action='store_true',
                     help='output version information and exit')
+
+
+def dfs(v):
+
+    color[v] = 'grey'
+
+    for u in structure[v]:
+        if color[u] == 'white':
+            dfs(u)
+
+    color[v] = 'black'
+    done.append(v)
+
+
+def topological_sort(text):
+
+    text = re.sub('\s+|\n+', ' ', text)
+    lst = text.split()
+
+    global structure
+    structure = defaultdict(list)
+
+    for i in range(0, len(lst), 2):
+        structure[lst[i]].append(lst[i + 1])
+
+    global color
+    color = defaultdict(lambda: 'white')
+
+    global done
+    done = []
+
+    dfs(lst[0])
+
+    return list(reversed(done))
 
 
 def main():
@@ -21,3 +58,7 @@ def main():
         return 0
 
     text = args.infile.read()
+
+    for elem in topological_sort(text):
+        print(elem)
+
